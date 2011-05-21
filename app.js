4266ec88deccc658176b2a850f53db5622459970
Grey <conancat@gmail.com>
2011-05-21 17:50:18 +0800

    Done skeleton page template

--- app.js
+++ app.js
@@ -29,9 +29,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Supermom'
-  });
+  res.render('index');
 });
 
 // Only listen on $ node app.js
